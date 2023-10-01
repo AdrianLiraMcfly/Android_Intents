3397b40c8c437ce4559c7fc00d5db6f1d19ff7d4
adrian <jorge.liralopez11@gmail.com>
2023-09-30 19:53:56 -0600

    otro avance porque ya ando pedo y se me ocurrieron cosas

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,9 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Button btnn = findViewById( R.id.btnn );
+        Button btng = findViewById( R.id.btng );
+        Button btnc = findViewById( R.id.btnc );
+        Button btnno = findViewById( R.id.btnn );
 
         btnn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +31,58 @@ public class MainActivity2 extends AppCompatActivity {
 
                 if (navega.resolveActivity(getPackageManager()) != null) {
                     startActivity(navega);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No fue posible realizar la opcion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double latitud =25.532851168559922;
+                double longitud=-103.32174354857497;
+
+                Uri abrirmaps = Uri.parse("google.navigation:q=" + latitud + "," + longitud);
+                Intent maps= new Intent(Intent.ACTION_VIEW, abrirmaps);
+                maps.setPackage("com.google.android.apps.maps");
+                if (maps.resolveActivity(getPackageManager()) != null) {
+                    startActivity(maps);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No fue posible realizar la opcion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent calc = new Intent( Intent.ACTION_MAIN );
+                calc.setAction(Intent.ACTION_MAIN);
+                calc.addCategory(Intent.CATEGORY_APP_CALCULATOR);
+
+                PackageManager pm = getPackageManager();
+                ResolveInfo ri = pm.resolveActivity(calc, PackageManager.MATCH_DEFAULT_ONLY);
+
+                if (ri != null) {
+                    startActivity(calc);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No fue posible realizar la opcion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+            btnn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent contact = new Intent( Intent.ACTION_MAIN );
+                contact.setData(android.provider.ContactsContract.Contacts.CONTENT_URI);
+
+                PackageManager pm = getPackageManager();
+                ResolveInfo ri = pm.resolveActivity(contact, PackageManager.MATCH_DEFAULT_ONLY);
+
+                if( ri != null ) {
+                    startActivity(contact);
                 } else {
                     Toast.makeText(getApplicationContext(), "No fue posible realizar la opcion", Toast.LENGTH_SHORT).show();
                 }
