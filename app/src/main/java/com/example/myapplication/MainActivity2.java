5477096c4affe79d5870e10cc677aebd529b73bc
adrian <jorge.liralopez11@gmail.com>
2023-09-30 19:09:01 -0600

    avance bueno antes de irme a pistear

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button btnn = findViewById( R.id.btnn );
+
+        btnn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.google.com");
+                Intent navega = new Intent( Intent.ACTION_VIEW, uri );
+
+                if (navega.resolveActivity(getPackageManager()) != null) {
+                    startActivity(navega);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No fue posible realizar la opcion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
